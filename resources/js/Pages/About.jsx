71e0689f0d7ac55cdb3103e89317bb11ce9dc41f
Yasuhiro WATANABE <info@nabeyasu.com>
2024-10-10 15:03:24 +0900

    Add About and Home pages

---
+++ resources/js/Pages/About.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function About() {
+    return (
+        <div>
+            <h1>About Us</h1>
+            <p>This is the About page.</p>
+        </div>
+    );
+}
